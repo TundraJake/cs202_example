ad69b0268294583a29651d54fdcdc6665531a54d
develjacob <mcjacob61@gmail.com>
2018-03-05 18:14:56 -0900

    fix file

--- main.cpp
+++ main.cpp
@@ -8,10 +8,13 @@ Review:
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "money.cpp"
-TEST_CASE("Money Testing"){
+
+TEST_CASE("Money Testing -- Simple Init"){
 
 	Money a(5,5);
 	REQUIRE(a.getDollar() == 5);
 	REQUIRE(a.getChange() == 5);
+	REQUIRE(a.getBalance() == "$5.05");
+	cout <<"wtf" << endl;
 
 }
