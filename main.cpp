b6edcd35cbb32c9b99cb78a2b07bbf873f66b39f
develjacob <mcjacob61@gmail.com>
2018-03-05 18:55:59 -0900

    add printLine function, cleaner printout

--- main.cpp
+++ main.cpp
@@ -9,36 +9,46 @@ Review:
 #include "catch.hpp"
 #include "money.cpp"
 
+void printLine(){
+	cout << "\n==================\n" << endl;
+}
+
 TEST_CASE("Money Testing -- Simple Init"){
 
-	Money a(5,5);
-	REQUIRE(a.getDollar() == 5);
-	REQUIRE(a.getChange() == 5);
-	REQUIRE(a.getBalance() == "$5.05");
-
+	{
+		Money a(5,5);
+		REQUIRE(a.getDollar() == 5);
+		REQUIRE(a.getChange() == 5);
+		REQUIRE(a.getBalance() == "$5.05");
+	}
+	printLine();
 }
 
 TEST_CASE("Money Testing -- Alter Data"){
+	
+	{
+		Money a(5,5);
+		cout << "Enter values 3 and 10 to pass following tests..." << endl;
+		a.setNewBalance();
 
-	Money a(5,5);
-	cout << "Enter values 3 and 10 to pass following tests..." << endl;
-	a.setNewBalance();
-
-	REQUIRE(a.getDollar() == 3);
-	REQUIRE(a.getChange() == 10);
-	REQUIRE(a.getBalance() == "$3.10");
-
+		REQUIRE(a.getDollar() == 3);
+		REQUIRE(a.getChange() == 10);
+		REQUIRE(a.getBalance() == "$3.10");
+	}
+	printLine();
 }
 
 TEST_CASE("Operator Overloading -- Simple Test"){
 
-	Money a(5,5);
-	Money b(5,5);
-	Money c;
+	{
+		Money a(5,5);
+		Money b(5,5);
+		Money c;
 
-	c = a + b;
-	REQUIRE(c.getDollar() == 10);
-	REQUIRE(c.getChange() == 10);
-	REQUIRE(c.getBalance() == "$10.10");
-
+		c = a + b;
+		REQUIRE(c.getDollar() == 10);
+		REQUIRE(c.getChange() == 10);
+		REQUIRE(c.getBalance() == "$10.10");
+	}
+	printLine();
 }
